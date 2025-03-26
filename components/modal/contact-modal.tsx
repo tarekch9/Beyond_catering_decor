9854141417b71c49a10efa554027ffb65a95a77b
unknown <tachaani@protonmail.com>
2025-03-26 12:31:40 +0100

    +

--- components/modal/contact-modal.tsx
+++ components/modal/contact-modal.tsx
@@ -9,7 +9,12 @@ const Home = () => {
   const [step, setStep] = useState(1);
   const [file, setFile] = useState(null);
 
-  const { register, handleSubmit, formState: { errors }, reset } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
   const handleClose = () => {
     reset();
@@ -56,248 +61,40 @@ const Home = () => {
       </button>
 
       <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="mx-auto bg-black p-20 rounded-lg shadow text-[#f2f1f0]/90"
-        >
-          {step === 1 && (
-            <div>
-              <h2 className="text-xl mb-4 font-serif text-[#cdab6e]">
-                Step 1: Contact Information
-              </h2>
-
-              <div className="mb-4">
-                <label className="block mb-1 text-white">Name:</label>
-                <div className="bg-white text-black border rounded">
-                  <input
-                    type="text"
-                    {...register("name", { required: "Name ist erforderlich" })}
-                    className="w-full p-2 border-none"
-                  />
-                </div>
-                {errors.name?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.name.message}
-                  </p>
-                )}
-              </div>
-
-              <div className="mb-4">
-                <label className="block mb-1 text-white">E-Mail:</label>
-                <div className="bg-white text-black border rounded">
-                  <input
-                    type="email"
-                    {...register("email", {
-                      required: "E-Mail ist erforderlich",
-                      pattern: {
-                        value:
-                          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                        message: "Ungültige E-Mail-Adresse",
-                      },
-                    })}
-                    className="w-full p-2 border-none"
-                  />
-                </div>
-                {errors.email?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-
-              <div className="mb-4">
-                <label className="block mb-1 text-white">Tel.:</label>
-                <div className="bg-white text-black border rounded">
-                  <input
-                    type="text"
-                    {...register("telnumber", {
-                      required: "Telefonnummer ist erforderlich",
-                      pattern: {
-                        value: /^[0-9+-]+$/,
-                        message: "Nur Zahlen und + erlaubt",
-                      },
-                    })}
-                    className="w-full p-2 border-none"
-                  />
-                </div>
-                {errors.telnumber?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.telnumber.message}
-                  </p>
-                )}
-              </div>
-
-              <button
-                type="button"
-                onClick={nextStep}
-                className="bg-black border border-[#cdab6e] hover:bg-[#94794a] text-white p-2 rounded mt-4"
+        <div className="mx-auto bg-black p-16 rounded-lg shadow text-[#f2f1f0]/90 max-w-2xl overflow-y-auto">
+          <h1 className="text-4xl flex justify-center font-serif text-[#cdab6e] pb-4">
+            Contact Us
+          </h1>
+          <p className="font-sans font-extralight text-center">
+            Want to get in touch with us and book us for one of your events?
+            Just write us an E-Mail with the
+            <strong> "Number of Guests"</strong>, <strong> "Location"</strong>,
+            <strong> "Event-Date"</strong>,<strong> "Budget"</strong> and you <strong> "Food Selection"</strong> and if you want as service type a
+            <strong> "Full service"</strong> or a <strong> "Buffet Service"</strong>. Feel also free to send us
+            Photos of the venue, so that we can better prepare your personal,
+            unique Event :)
+          </p>
+          <section className="mb-8">
+            <div className="flex flex-wrap gap-3 py-4 place-content-center">
+              <a
+                href="#specialised"
+                className="text-sm font-light bg-black border border-[#cdab6e] text-white px-6 py-3 rounded-md hover:bg-[#cdab6e] hover:text-black transition-colors duration-300"
               >
-                Weiter
-              </button>
+                E-Mail
+              </a>
+              <a
+                href="#specialised"
+                className="text-sm font-light bg-[#94794a] border border-[#cdab6e] text-white px-6 py-3 rounded-md hover:bg-[#cdab6e] hover:text-black transition-colors duration-300"
+              >
+                Telephone
+              </a>
             </div>
-          )}
-          {step === 2 && (
-            <div className="text-[#f2f1f0]/90">
-              <h2 className="text-xl font-serif mb-4 text-[#cdab6e]">
-                Step 2: Catering Information
-              </h2>
-              Location:
-              <label className="block mb-2 bg-white text-black border rounded">
-                <input
-                  type="text"
-                  {...register("location", {
-                    required: "Location is required",
-                  })}
-                  className="w-full p-2 border rounded"
-                />
-                {errors.location?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.location.message}
-                  </p>
-                )}
-              </label>
-              Event-date:
-              <label className="block mb-2 bg-white text-black border rounded">
-                <input
-                  type="date"
-                  {...register("date", { required: "Date is required" })}
-                  className="w-full p-2 border rounded"
-                />
-                 {errors.date?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.date.message}
-                  </p>
-                )}
-              </label>
-              Budget (approximately):
-              <label className="block mb-2 bg-white text-black border rounded">
-                <input
-                  type="text"
-                  {...register("budget", {
-                    required: "Please give us your budget",
-                    pattern: {
-                      value: /^[0-9]+$/,
-                      message: "Only numbers allowed",
-                    },
-                  })}
-                  className="w-full p-2 border rounded"
-                />
-                 {errors.budget?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.budget.message}
-                  </p>
-                )}
-              </label>
-              Preferences (Menu, etc.):
-              <label className="block mb-2 bg-white text-black rounded">
-                <textarea
-                  {...register("preferences", {
-                    required: "Please tell us about your preferences",
-                  })}
-                  className="w-full p-2 rounded"
-                  rows="3"
-                />
-                 {errors.preferences?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.preferences.message}
-                  </p>
-                )}
-              </label>
-              <div className="flex justify-between mt-4">
-                <button
-                  type="button"
-                  onClick={prevStep}
-                  className="bg-black border border-[#cdab6e] rounded-l hover:bg-[#94794a] text-white p-2 rounded mt-4"
-                >
-                  Zurück
-                </button>
-                <button
-                  type="button"
-                  onClick={nextStep}
-                  className="bg-black border border-[#cdab6e] rounded-l hover:bg-[#94794a] text-white p-2 rounded mt-4"
-                >
-                  Weiter
-                </button>
-              </div>
-            </div>
-          )}
-
-          {step === 3 && (
-            <div className="text-[#f2f1f0]/90">
-              <h2 className="text-xl font-serif mb-4 text-[#cdab6e]">
-                Step 3: Catering Information
-              </h2>
-              {/* Service type */}
-              Service type:
-              <label className="block mb-2 bg-white text-black border rounded">
-                <select
-                  {...register("serviceType", {
-                    required: "Service type is required",
-                  })}
-                >
-                  <option value="Full service">Full service</option>
-                  <option value="Buffet service">Buffet service</option>
-                </select>
-                {errors.serviceType?.message && (
-                  <p className="text-red-500 mt-1 text-sm">
-                    {errors.serviceType.message}
-                  </p>
-                )}
-              </label>
-              {/* Food selection */}
-              Food selection:
-              <label className="block mb-2 bg-white text-black border rounded">
-                <select
-                  {...register("foodSelection", {
-                    required: "Food selection is required",
-                  })}
-                >
-                  <option value="Breakfast Buffet">Breakfast Buffet</option>
-                  <option value="Breakfast Basket">Breakfast Basket</option>
-                  <option value="Canapés">Canapés</option>
-                  <option value="Candy bars">Candy bars</option>
-                  <option value="Charcuterie boards">Charcuterie boards</option>
-                  <option value="Fine Dining">Fine Dining</option>
-                  <option value="Fruit Platter">Fruit Platter</option>
-                  <option value="Salads">Salads</option>
-                </select>
-                {errors.foodSelection && (
-                  <p className="text-red-500">{errors.foodSelection.message}</p>
-                )}
-              </label>
-              {/* Bild-Upload */}
-              <label className="block mb-2 mt-4 bg-white text-black border rounded">
-                Upload Image:
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={(e) => setFile(e.target.files[0])}
-                  className="w-full p-2 border rounded"
-                />
-              </label>
-              {/* Note */}
-              Note: If your event is outside the London area,<br></br> we do
-              offer travel services, starting at a £5k<br></br> minimum,
-              available upon request
-              {/* Buttons */}
-              <div className="flex justify-between mt-4">
-                <button
-                  type="button"
-                  onClick={prevStep}
-                  className="bg-black border border-[#cdab6e] rounded-l hover:bg-[#94794a] text-white p-2 rounded mt-4"
-                >
-                  Zurück
-                </button>
-                <button
-                  type="submit"
-                  className="bg-black border border-[#cdab6e] rounded-l hover:bg-[#94794a] text-white p-2 rounded mt-4"
-                >
-                  Absenden
-                </button>
-              </div>
-            </div>
-          )}
-        </form>
+            <p className="font-sans font-extralight text-center">
+              Note: If your event is outside the London area, we do offer travel
+              services, starting at a £5k minimum, available upon request.
+            </p>
+          </section>
+        </div>
       </Modal>
     </div>
   );
--- components/privacypolicy.tsx
+++ components/privacypolicy.tsx
@@ -9,13 +9,16 @@ const PrivacyPolicy = () => {
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">1. Introduction</h2>
         <p>
-          Welcome to our Privacy Policy. Your privacy is critically important to us. This policy
-          explains what information we collect, how we use it, and your rights regarding your data.
+          Welcome to our Privacy Policy. Your privacy is critically important to
+          us. This policy explains what information we collect, how we use it,
+          and your rights regarding your data.
         </p>
       </section>
 
       <section className="mb-6">
-        <h2 className="text-xl font-semibold mb-2">2. Information We Collect</h2>
+        <h2 className="text-xl font-semibold mb-2">
+          2. Information We Collect
+        </h2>
         <ul className="list-disc pl-6">
           <li>Personal Information (e.g., name, email address)</li>
           <li>Usage Data (e.g., pages visited, time spent on site)</li>
@@ -24,7 +27,9 @@ const PrivacyPolicy = () => {
       </section>
 
       <section className="mb-6">
-        <h2 className="text-xl font-semibold mb-2">3. How We Use Your Information</h2>
+        <h2 className="text-xl font-semibold mb-2">
+          3. How We Use Your Information
+        </h2>
         <ul className="list-disc pl-6">
           <li>To provide and maintain our services</li>
           <li>To improve our website and user experience</li>
@@ -35,15 +40,17 @@ const PrivacyPolicy = () => {
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">4. Your Rights</h2>
         <p>
-          You have the right to access, update, or delete your personal information. If you wish
-          to exercise these rights, please contact us at support@example.com.
+          You have the right to access, update, or delete your personal
+          information. If you wish to exercise these rights, please contact us
+          at support@example.com.
         </p>
       </section>
 
       <section className="mb-6">
         <h2 className="text-xl font-semibold mb-2">5. Contact Us</h2>
         <p>
-          If you have any questions about this Privacy Policy, please contact us at:
+          If you have any questions about this Privacy Policy, please contact us
+          at:
           <br /> Email: support@example.com
         </p>
       </section>
